cb171a7e09bb08fe2b64b7e87dee939f5dc4bf56
Ajay Katte <ajaymkatte95@gmail.com>
2018-02-25 14:47:23 +0530

    0.0.4
    Handles response errors, and encoding errors. Formatting changes.

--- wciwt/__init__.py
+++ wciwt/__init__.py
@@ -8,13 +8,24 @@ def wciwt():
 		print ('Usage: wciwt "movie or show title"')
 		exit()
 
-	json = (requests.get("https://wciwt.in/api/?format=json&query=" + query)).json()
+	try:
+		json = (requests.get("https://wciwt.in/api/?format=json&query=" + query))
+	except:
+		print("Error during requesting, try again after sometime.")
+		exit()
+
+	if (json.status_code) != 200:
+		print ("Error in request.")
+		exit()
+
+	json = json.json()
+
 	if (json['total_results'] == 0):
-		print ("No such title in our database")
+		print ("No such title in our database.")
 	else:
 		for service in json['results']:
 			if len(json['results'][service]) != 0:
-				print ("\n{}: {}\n".format(service.upper(), ', '.join(str(x) for x in json['results'][service])))
+				print ("\n{}:\n{}\n".format(service.upper(), '\n'.join(str(x.encode('utf-8')) for x in json['results'][service])))
 
 if __name__ == "__main__":
 	wciwt()
